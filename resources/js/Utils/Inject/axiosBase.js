206f5a265b501054b02235859289e19b8a5111fb
Epba <ekopurnomo.office@gmail.com>
2023-03-08 02:09:30 +0700

    move apikey to env

--- resources/js/Utils/Inject/axiosBase.js
+++ resources/js/Utils/Inject/axiosBase.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
     install: (app, options) => {
         const ax = axios.create({
-            baseURL: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=jakarta,id&APPID=2135a1f2258ebdcf65e42628d1da8ebf&units=metric',
+            baseURL: `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=jakarta,id&APPID=${import.meta.env.VITE_APP_WEATHER}&units=metric`,
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Origin': '*',
